c713428bf17995ea00b2c53824b6a81720f526c4
Matthew Seddon <matthew.seddon@protonmail.ch>
2025-04-29 11:20:02 +0200

    fix compile redefinition error

--- modular/cmpstd/service/service.cpp
+++ modular/cmpstd/service/service.cpp
@@ -1,22 +1,19 @@
 #include <cmpstd/service/service.h>
 #include <grpcpp/grpcpp.h>
-#include <iostream>
+#include <grpcpp/support/status.h>
 #include "proto/cmpstd.grpc.pb.h"
 #include "proto/cmpstd.pb.h"
 
 namespace cmpstd { 
 namespace service { 
-class StandardizeMoleculeImpl final : public StandardizeMolecule::Service {
-    public:
-      grpc::Status runStandardize(grpc::ServerContext* context, 
+grpc::Status StandardizeMoleculeImpl::runStandardize(grpc::ServerContext* context, 
                                   const Molecule* request,
-                                  Molecule* response) override {
+                                  Molecule* response) {
     std::cout << "Called runStandardize \n";
     response->set_id(request->id());
     response->set_smiles("SMILES");    
 
     return grpc::Status::OK;
     }
-};
 } //namespace service
 } //namespace cmpstd
